77ce39960d7f66c205f998c016a21eb2839ec792
amenallah122 <garmaiamenallah@gmail.com>
2023-10-13 03:30:47 -0700

    betty coding style

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - a function that displays a message 
- * void 
- * Return:0
- */
+* main - a function that displays a message
+* Return:0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
