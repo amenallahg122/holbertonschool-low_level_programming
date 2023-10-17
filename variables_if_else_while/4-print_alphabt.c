56f689b223d6dc72835806a5a1681d92d4958448
amenallah122 <garmaiamenallah@gmail.com>
2023-10-17 03:58:08 -0700

    alphabet -e and q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+	if (alphabet == !e || q)
+	{
+	putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
