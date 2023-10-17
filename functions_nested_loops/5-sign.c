e286e6ca81de6213ca6006462dc1e74f93044046
amenallah122 <garmaiamenallah@gmail.com>
2023-10-17 15:59:48 +0200

    sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_sign -  prints sign of number
+ * Return: Always 0 (Success)
+ */
+
+int _print_sign(int n)
+{
+    if (0 > n)
+        ;
+    {
+        _putchar("+") return (1);
+    }
