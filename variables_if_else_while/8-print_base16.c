29defb0be4141688b701be7341acbe9ac3be465e
amenallah122 <garmaiamenallah@gmail.com>
2023-10-17 05:14:12 -0700

    print base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main - prints numbers of base 16 in lowercase
+  *Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 15; n++)
+	{
+		printf("%x", n);
+	}
+	printf("\n");
+	return (0);
+}
