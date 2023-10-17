2565627ffabff2d9465f57255b0ddefcdb3efb3c
amenallah122 <garmaiamenallah@gmail.com>
2023-10-17 15:36:04 +0200

    is alpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ * @c : integer
+ */
+
+int _islower(int c)
+{
+
+	if (122 >= c >= 65)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
