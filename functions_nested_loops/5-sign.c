4cd3fb67ddd5cd3ef8536dac0f26663c09df2ac4
amenallah122 <garmaiamenallah@gmail.com>
2023-10-18 10:41:28 +0200

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,18 +9,17 @@ int _print_sign(int n)
 {
 	if (0 > n)
 	{
-		_putchar(+);
-		return (1);
+		_putchar(43);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar(0);
+		_putchar(45);
 		return (0);
 	}
 	else
 	{
-		_putchar(-);
+		_putchar(48);
 		return (-1);
 	}
 }
