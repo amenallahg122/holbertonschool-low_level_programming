6d5b9c68e38602c673963f564ca41c04ec8f3167
amenallah122 <garmaiamenallah@gmail.com>
2023-10-19 15:24:53 +0200

    line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
   * print_line - Draws a straight line according to parameter
   * @n: The number of lines to draw
@@ -11,15 +12,15 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 	else
 	{
 		for (x = 0; x < n; x++)
 		{
-			_putchar(95);
+			putchar(95);
 		}
 
-		_putchar('\n');
+		putchar('\n');
 	}
 }
