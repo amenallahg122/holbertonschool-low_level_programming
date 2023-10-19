875a4a6baceb9a4859e45e9bf5b87651b92891fc
amenallah122 <garmaiamenallah@gmail.com>
2023-10-19 14:33:44 +0200

    mosy numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - Prints from 0 to 9 except 2 and 4
+ *
+ * Return: Always (Success)
+ */
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9' ; a++)
+		if (a != '2' && a != '4')
+	{
+			putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
