71b436170e3e3ac4b1e8381ab562b0135267a54d
amenallah122 <garmaiamenallah@gmail.com>
2023-10-19 14:10:16 +0200

    multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - multiplies two integers
+ * @A: integer
+ * @B: integer
+ * m: integer
+ * Return: result of the addition
+ */
+
+int mul(int A, int B)
+{
+	int m = 0;
+
+	m = A * B;
+	return (m);
+}
