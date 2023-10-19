1ba8937e94f3eebb64c52ca0769878d4d117d6ff
amenallah122 <garmaiamenallah@gmail.com>
2023-10-19 15:42:12 +0200

    diagonal line

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal - prints diagonal line
+ * @n : integer
+ * Return: void (Success) .
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; i > j; j++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
