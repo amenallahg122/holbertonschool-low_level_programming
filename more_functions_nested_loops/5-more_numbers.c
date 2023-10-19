dda236cb905960b1e88c763273f80625ce91aa51
amenallah122 <garmaiamenallah@gmail.com>
2023-10-19 15:17:08 +0200

    more numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,10 +14,10 @@ void more_numbers(void)
 	{
 		for (a = 0 ; a <= 14 ; a++)
 		{
-            if (a > 9)
-            {
-                putchar((a / 10) + '0');
-            }
+			if (a > 9)
+			{
+				putchar((a / 10) + '0');
+			}
 			putchar((a % 10) + '0');
 		}
 		putchar('\n');
