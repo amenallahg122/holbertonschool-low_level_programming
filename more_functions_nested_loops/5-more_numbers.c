3c45f1ff1e170a2e52b39e28ad42269ae91eb395
amenallah122 <garmaiamenallah@gmail.com>
2023-10-19 15:09:31 +0200

    more numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,19 @@
 
 void more_numbers(void)
 {
-    char a;
-    int i;
+	int a;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        while (a <= 14)
-        {
-            for (a = '0'; a++)
+	for (i = 0; i < 10; i++)
+	{
+		for (a = '0'; a <= '14'; a++)
+		{
+            if (a > 9)
             {
-                putchar(a);
+                _putchar((y / 10) + '0');
             }
-            putchar('\n');
-        }
-    }
+			putchar((a % 10) + '0');
+		}
+		putchar('\n');
+	}
 }
