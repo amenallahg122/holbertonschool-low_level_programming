320170f82019b6bf4dd6aad69199e75353042310
amenallah122 <garmaiamenallah@gmail.com>
2023-10-19 14:29:51 +0200

    most numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,19 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * main - Prints the alphabet except e and q
- *
- * Return: Always (Success)
+ * print_numbers -  prints numbers from 0 to 9
+ * Return: Always  (Success)
  */
 
-void print_most_numbers(void)
+void print_numbers(void)
+{
 	char a;
 
-	for (a = '0'; a <= '9' ; al++)
-		if (a != '2' && a != '4')
+	for (a = '0'; a <= '9'; a++)
 	{
-			putchar(a);
+		putchar(a);
 	}
+
 	putchar('\n');
-	return (0);
 }
