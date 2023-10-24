6a791a3a1866aaf96fc7a60f95c226b14ef56354
amenallah122 <garmaiamenallah@gmail.com>
2023-10-24 10:37:31 +0200

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - returns length of string
+ * @s: string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+		_putchar(i);
+	return (0);
+}
