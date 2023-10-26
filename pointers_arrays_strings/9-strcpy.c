9d0b5c1e49ad2f1dd6fec53136db80e48a7f4852
amenallah122 <garmaiamenallah@gmail.com>
2023-10-26 09:12:14 +0200

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+
+/**
+ * _strcpy - 
+ * @dest: destination
+ * @src: source
+ * Return: 
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
