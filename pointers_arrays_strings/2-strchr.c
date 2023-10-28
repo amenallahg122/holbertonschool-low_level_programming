4c3885c57ecb2d9c457f8149d632c4008bfa29e1
amenallah122 <garmaiamenallah@gmail.com>
2023-10-29 00:24:10 +0200

    2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: the string
+ * @c: the character
+ *
+ * Return: a pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
