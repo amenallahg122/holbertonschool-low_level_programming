b802b93a0ef11151318c44394be6ceb908329c0e
amenallah122 <garmaiamenallah@gmail.com>
2023-10-28 16:50:30 +0200

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -18,13 +18,13 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (a < n && src[a])
 	{
-		dest[a] = src[a]
+		dest[a] = src[a];
 		a++;
 	}
 
 	while (a < n)
 	{
-		dest[a] = '\0'
+		dest[a] = '\0';
 		a++;
 	}
 
