bc2a7186cb80160e889759334f2ed6c20947d876
amenallah122 <garmaiamenallah@gmail.com>
2023-10-31 18:17:52 +0100

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
- * _puts_recursion - puts
+ * _puts_recursion - prints a string
  * @s: string
- * Return: i
+ * Return: nothing
  */
+
 void _puts_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
