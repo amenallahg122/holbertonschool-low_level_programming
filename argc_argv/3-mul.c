b2398ce0f99f5511923c7f3ecf713f43d4f0469f
amenallah122 <garmaiamenallah@gmail.com>
2023-11-02 12:23:07 +0100

    3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: int
+ * @argv: string
+ *
+ * Return: 0 or 1
+ */
+
+int main(int argc, char **argv)
+{
+	int a, b, diff;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	diff = a * b;
+
+	printf("%i\n", diff);
+
+	return (0);
+}
