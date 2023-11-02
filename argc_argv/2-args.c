d63c069e81b2a2b828be0acb473709eab8813b25
amenallah122 <garmaiamenallah@gmail.com>
2023-11-02 11:33:33 +0100

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - entry point
+* @argc: int
+* @argv: array
+* Return: 0
+*/
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	while (i >= 0)
+		printf("%s\n", argv[i]);
+		return (0);
+}
