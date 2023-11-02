0fd18660c9d0bba5ebac4c1eb452c651326917d8
amenallah122 <garmaiamenallah@gmail.com>
2023-11-02 11:25:55 +0100

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - entry point
+* @argc: int
+* @argv: array
+* Return: 0
+*/
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%s\n", argc);
+	return (0);
+}
