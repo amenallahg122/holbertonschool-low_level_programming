9e56c37773701f81899cbcdf066a92655d4491e7
amenallah122 <garmaiamenallah@gmail.com>
2023-11-02 10:53:05 +0100

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - entry point
+* @argc: int
+* @argv: array
+* Return: 0
+*/
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
