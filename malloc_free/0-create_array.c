443a36492b442899d7d10bd59065b021955eac62
amenallah122 <garmaiamenallah@gmail.com>
2023-11-06 11:39:07 +0100

    0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_array - creates an array of characters
+ * @c: The character
+ * @size: the size of the array
+ * Return: NULL if size is 0 and array if not
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *array = malloc(1 * size);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+
+		{
+			array[i] = c
+		}
+		return (array);
+	}
+}
