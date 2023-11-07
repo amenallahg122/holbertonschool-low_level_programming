9efe2d1a77dc867997efb65e4d0bb88a0d1ce02c
amenallah122 <garmaiamenallah@gmail.com>
2023-11-07 10:33:09 +0100

    1-strdup.c*

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strdup - duplicates string
+ * @str: string
+ * Return: string
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	unsigned int i;
+	int n;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	n = strlen(str);
+	s = malloc((n + 1) * sizeof(char));
+	if (s != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+			s[i] = str[i];
+	}
+	else
+	{
+		return (NULL);
+	}
+	s[i] = '\0';
+	return (s);
+}
