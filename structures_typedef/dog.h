c593c2343d6e821332f46bbb7745ae1561feb095
amenallah122 <garmaiamenallah@gmail.com>
2023-11-13 11:34:15 +0100

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif DOG
