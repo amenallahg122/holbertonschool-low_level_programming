5036d26c59a4412c3f74043b80506c0286cd44e8
amenallah122 <garmaiamenallah@gmail.com>
2023-11-30 10:25:18 +0100

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_list - prints all elements of a list
+ * @h: pointer
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d]%s\n", h->len, h->str);
+			h = h->next;
+		}
+		return (i);
+	}
+}
