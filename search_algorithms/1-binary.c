21778a36b34cb6873716dceecc857dcf730775a9
amenallah122 <garmaiamenallah@gmail.com>
2024-01-18 21:07:40 +0100

    1-binary.c

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Searches for a value in an array
+ * @array: the array
+ * @size: size of the array
+ * @value: value to search for
+ * Return: val
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, left, right;
+
+	if (array == NULL)
+		return (-1);
+
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+
+	return (-1);
+}
